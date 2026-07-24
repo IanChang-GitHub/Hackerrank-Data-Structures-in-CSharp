dbdaff45c5a4c619baed9d55c964cb3b950ec34b
Ian Chang <78878903+IanChang-GitHub@users.noreply.github.com>
2026-07-24 10:40:49 +0800

    Add files via upload

---
+++ Cycle Detection.cs
@@ -0,0 +1,135 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter) {
+        while (node != null) {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     * 解題策略:
+     * 1.使用兩個指標一個移動較快一個移動較慢
+     * 2.若無迴圈會直接抵達終點
+     * 3.若有迴圈快的指標先到達迴圈並在迴圈內繞圈，等慢的指標進入迴圈時快的指標必會從後面追上慢的指標
+     */
+    static bool hasCycle(SinglyLinkedListNode head) {
+        
+        SinglyLinkedListNode slow = head; //烏龜 
+        SinglyLinkedListNode fast = head; //兔子
+        
+        if (head == null || head.next == null)
+            return false;
+        while (fast != null && fast.next != null ) //還沒到達終點
+        {
+            slow = slow.next; //走一步
+            fast = fast.next.next; //走兩步
+            
+            if(slow == fast) //移動一次差距為一步，不管距離多少，只要有迴圈必會剛好追上
+                return true;
+        }
+        return false;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+          
+          	SinglyLinkedListNode extra = new SinglyLinkedListNode(-1);
+            SinglyLinkedListNode temp = llist.head;
+
+            for (int i = 0; i < llistCount; i++) {
+                if (i == index) {
+                    extra = temp;
+                }
+
+                if (i != llistCount-1) {
+                    temp = temp.next;
+                }
+            }
+      
+      		temp.next = extra;
+
+            bool result = hasCycle(llist.head);
+
+            textWriter.WriteLine((result ? 1 : 0));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
