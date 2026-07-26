db0faedffa83e0108ce74c086d53663bbf5bede8
Ian Chang <4a0g0902@stust.edu.tw>
2026-07-26 19:12:29 +0800

    Is This a Binary Search Tree?

--- Is This a Binary Search Tree.cs
+++ Is This a Binary Search Tree.cs
@@ -49,7 +49,8 @@ class Tree
     }
 }
 
-class Solution {
+class Solution 
+{
 
     /*
      * 解題策略:
@@ -60,11 +61,13 @@ class Solution {
      * 5.若節點的值超出範圍，則二元搜尋樹不合法
     */
 
-    public static bool checkBST(Node root) {
+    public static bool checkBST(Node root) 
+    {
         return CheckBSTHelper(root, int.MinValue, int.MaxValue); //上限與下限
     }
 
-    private static bool CheckBSTHelper(Node node, int min, int max) {
+    private static bool CheckBSTHelper(Node node, int min, int max) 
+    {
         if (node == null) 
         {
             return true;
@@ -79,7 +82,8 @@ class Solution {
                CheckBSTHelper(node.right, node.data, max);
     }
 
-    public static void Main(string[] args) {
+    public static void Main(string[] args) 
+    {
         
         string input = Console.In.ReadToEnd();
         string[] tokens = input.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
@@ -90,7 +94,8 @@ class Solution {
         int treeHeight = int.Parse(tokens[0]);
         
         Tree tree = new Tree();
-        for (int i = 1; i < tokens.Length; i++) {
+        for (int i = 1; i < tokens.Length; i++) 
+        {
             tree.values.Add(int.Parse(tokens[i]));
         }
         
