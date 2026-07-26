80bce5eee043741ebf5a1a54e687c39dce052a05
Ian Chang <4a0g0902@stust.edu.tw>
2026-07-27 01:57:45 +0800

    Queue using Two Stacks

---
+++ Queue using Two Stacks.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Queue
+{
+    private Stack<int> stackIn = new Stack<int>(); 
+    private Stack<int> stackOut = new Stack<int>();
+    
+    public void Enqueue(int x)
+    {
+        stackIn.Push(x);
+    }
+    public void Dequeue()
+    {
+        reverseStack();
+        stackOut.Pop();
+
+    }
+    public int Peek() //只看不取
+    {
+        reverseStack();
+        return stackOut.Peek();
+        
+    }
+    
+    public void reverseStack() //反轉排序到另一個stack
+    {
+        if (stackOut.Count == 0) //另一個stack沒item才做反轉
+        {
+            while(stackIn.Count > 0)
+            {
+                stackOut.Push(stackIn.Pop());
+            }
+        }
+    }
+}
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string input = Console.In.ReadToEnd();
+        string[] tokens = input.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        int q = int.Parse(tokens[0]);
+        int index =1;
+        
+        Queue queue = new Queue();
+        for (int i=0;i<q;i++)
+        {
+            int type = int.Parse(tokens[index++]);
+            if (type == 1)
+            {
+                int x = int.Parse(tokens[index++]);
+                queue.Enqueue(x);
+            }
+            else if(type == 2)
+            {
+                queue.Dequeue();
+            }
+            else if(type == 3)
+            {
+                Console.WriteLine(queue.Peek());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | Cycle Detection | https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem | 
 | Tree: Huffman Decoding | https://www.hackerrank.com/challenges/tree-huffman-decoding/problem | 
 | Is This a Binary Search Tree? | https://www.hackerrank.com/challenges/is-binary-search-tree/problem | 
-|  |  |
+| Queue using Two Stacks | https://www.hackerrank.com/challenges/queue-using-two-stacks/problem |
 |  |  |
 |  |  |
 |  |  |
